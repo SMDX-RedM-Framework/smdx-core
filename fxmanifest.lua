68a662fa79271db3d7fded43758fed1d167ba982
SMDX Development <emilsven2021@gmail.com>
2024-06-28 18:33:21 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,6 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'SMDX Development'
 description 'smdx-core'
 version '1.0.0'
 
