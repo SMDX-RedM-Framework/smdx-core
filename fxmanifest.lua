0faf652d06444361d4987b8af412f5006314e7c9
SMDX Development <emilsven2021@gmail.com>
2024-06-28 15:47:51 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,46 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'SMDX Development'
+description 'smdx-core'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'shared/locale.lua',
+    'locale/sv.lua',
+    'locale/*.lua',
+    'shared/*.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/functions.lua',
+    'client/loops.lua',
+    'client/events.lua',
+    'client/drawtext.lua',
+    'client/prompts.lua',
+     'client/noclip.lua',
+    'client/pvp.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+    'server/functions.lua',
+    'server/player.lua',
+    'server/events.lua',
+    'server/commands.lua',
+    'server/exports.lua',
+    'server/debug.lua',
+    'server/versionchecker.lua'
+}
+
+dependency {
+    'oxmysql',
+    'ox_lib'
+}
+
+lua54 'yes'
