28f154421aa8c30a325064d751e0ac2d77d3d7a6
SMDX Development <emilsven2021@gmail.com>
2024-06-28 17:12:45 +0200

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,15 @@
+SMDXCore = {}
+SMDXCore.Config = SMDXConfig
+SMDXCore.Shared = SMDXShared
+SMDXCore.ClientCallbacks = {}
+SMDXCore.ServerCallbacks = {}
+
+exports('GetCoreObject', function()
+    return SMDXCore
+end)
+
+-----------------------------------------------------------------------
+
+-- To use this export in a script instead of manifest method
+-- Just put this line of code below at the very top of the script
+-- local SMDXCore = exports['smdx-core']:GetCoreObject()
